1f01973a06a998376f36295ac78d495dd5ff5c2f
Mark <shevchenko.mark@e-u.edu.ua>
2025-11-13 02:44:57 +0200

    Rebuild docs with correct script path

--- dashboard-p8/vite.config.js
+++ dashboard-p8/vite.config.js
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react'
 
 export default {
   plugins: [react()],
-  base: '/dashboard-p8/',   // назва репо на GitHub
+  base: '/dashboard-p8/',
   build: {
     outDir: 'docs',
   },
