3c4d7e6e56307235e33785bbd5294417f270e6ff
Mark <shevchenko.mark@e-u.edu.ua>
2025-11-13 02:18:09 +0200

    Build for GitHub Pages

--- dashboard-p8/vite.config.js
+++ dashboard-p8/vite.config.js
@@ -1,11 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// ЗАМІНИ 'dashboard-p8' на точну назву свого репозиторію на GitHub
 export default defineConfig({
   plugins: [react()],
   base: '/dashboard-p8/',
-  build: {
-    outDir: 'docs',
-  },
+  build: { outDir: 'docs' },
 })
