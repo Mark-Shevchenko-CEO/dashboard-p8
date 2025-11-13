8f7ef1b2a06ad35c659d50e314838154f359c17b
Mark <shevchenko.mark@e-u.edu.ua>
2025-11-13 20:07:20 +0200

    Build dashboard to root docs for GitHub Pages

--- dashboard-p8/vite.config.js
+++ dashboard-p8/vite.config.js
@@ -2,8 +2,8 @@ import react from '@vitejs/plugin-react'
 
 export default {
   plugins: [react()],
-  base: '/dashboard-p8/',   // ІМ'Я РЕПОЗИТОРІЮ
+  base: '/dashboard-p8/',          // назва репозиторію
   build: {
-    outDir: 'docs',         // куди Vite покладе збірку
+    outDir: '../docs',             // ВАЖЛИВО: збірка в корінь репо, не в dashboard-p8/docs
   },
 }
