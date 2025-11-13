5ef73803077e7e8c41f80e79e2002b26a6691563
Mark <shevchenko.mark@e-u.edu.ua>
2025-11-13 02:19:52 +0200

    Fix vite.config and build for Pages

--- dashboard-p8/vite.config.js
+++ dashboard-p8/vite.config.js
@@ -1,5 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
   base: '/dashboard-p8/',
-  build: { outDir: 'docs' },
+  build: {
+    outDir: 'docs',
+  },
 })
