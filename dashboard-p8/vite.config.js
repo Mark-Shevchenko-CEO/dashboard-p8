ba5ca67ae3815574f1b7a6eece4a98be7f10fa60
Mark <shevchenko.mark@e-u.edu.ua>
2025-11-13 02:37:48 +0200

    Rebuild docs for GitHub Pages

--- dashboard-p8/vite.config.js
+++ dashboard-p8/vite.config.js
@@ -1,9 +1,8 @@
 import react from '@vitejs/plugin-react'
 
-// без defineConfig, просто звичайний об'єкт
 export default {
   plugins: [react()],
-  base: '/dashboard-p8/',   // назва твого репозиторію на GitHub
+  base: '/dashboard-p8/',   // назва репо на GitHub
   build: {
     outDir: 'docs',
   },
